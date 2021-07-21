7dcd6a3fec59f24601cfb59c7c02d68a6497bbd5
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-07-21 19:42:11 -0400

    agregada la tienda y su configuracion

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { createStore,combineReducers,compose,applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+
+import pokeReducer from "./pokeDucks";
+
+const rootReducer = combineReducers({
+   pokemons:pokeReducer,
+//ej. usuario:usuarioReducer 
+}) // este es el que va a consumir nuestro componente
+
+const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;//configuracion de redux devtools
+
+export default function generateStore(){
+    const store = createStore(rootReducer, composeEnhacers(applyMiddleware(thunk)))
+    return store;
+}//se crea la tienda
