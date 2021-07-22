a6b72d8c1a7c36cd8960bd4b028e5f86ccf640c6
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-07-22 18:07:30 -0400

    se agrego la funcionalidad de anterior y tambien validacion de botones

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import { useDispatch,useSelector } from 'react-redux';
     //useDispatch nos va a servir para consumir nuestra accion ,useSelector nos servira para leer el state principal(array:[])
-import {obtenerPokemonsAccion,siguientePokemonAccion} from '../redux/pokeDucks';
+import {anteriorPokemonAccion, obtenerPokemonsAccion,siguientePokemonAccion} from '../redux/pokeDucks';
 
 const Pokemons = () => {
 
     const dispatch = useDispatch()
 
-    const pokemones = useSelector(store => store.pokemons.array)
+    const pokemones = useSelector(store => store.pokemons.results)
     console.log(pokemones)
+    const next = useSelector(store => store.pokemons.next)
+    const previous = useSelector(store => store.pokemons.previous)
 
     return (
         <div>
             <h1>LISTA DE POKEMONES UwU</h1>
-                             {/* //dispatch llama ala accion y la accion es una funcion por eso viene con parentesis */}
-          
+            <br/>
+            {
+                pokemones.length === 0 &&
+                
+                             /* //dispatch llama ala accion y la accion es una funcion por eso viene con parentesis */
+        
             <button onClick={()=>dispatch(obtenerPokemonsAccion())}>Trae el pokemon</button>
-
-                                                           {/*en el parametro de siguientePokemonAccion si pusieramos 20 y lo recibieramos en pokeDucks ese parametro daria tambien el mismo resultado , ya que esta seria la otra alternativa */}
-            <button onClick={()=>dispatch(siguientePokemonAccion())}>Siguiente</button>
-      <ul> {
+            }{
+                next &&
+                                                           /*en el parametro de siguientePokemonAccion si pusieramos 20 y lo recibieramos en pokeDucks ese parametro daria tambien el mismo resultado , ya que esta seria la otra alternativa */
+            <button onClick={()=>dispatch(siguientePokemonAccion())}>Siguiente</button>}
+            {previous &&
+            <button onClick={()=>dispatch(anteriorPokemonAccion())}>Anterior</button>}
+      <ul> 
+          {
               pokemones.map(item =>(
                   <li key={item.name} >{item.name}</li>
               ))
--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -2,8 +2,11 @@ import axios from "axios"
 
 // constantes
 const dataInicial ={
-    array : [],
-    offset:0
+    count:0,
+    next:null,
+    previous:null,
+    results : [],
+  
 }
 
 //types 
@@ -15,12 +18,9 @@ const SIGUIENTE_POKEMONS_EXITO = 'SIGUIENTE_POKEMONS_EXITO'
 export default function pokeReducer(state= dataInicial,action){
 switch(action.type){
 case OBTENER_POKEMONS_EXITO:
-    return {...state,array:action.payload} //se queda con el estado inicial y se le suma lo que le llega del payload
+    return {...state,...action.payload} //se queda con el estado inicial y se le suma lo que le llega del payload
 case SIGUIENTE_POKEMONS_EXITO:
-    return {...state,
-        array:action.payload.array,
-        offset: action.payload.offset
-}    
+    return {...state,...action.payload}
 default:
     return state
 }
@@ -32,16 +32,16 @@ default:
 export const obtenerPokemonsAccion = () => async (dispatch,getState)=>{
 
        
-       console.log('getSTate trae',getState().pokemons.offset)
-       const offset = getState().pokemons.offset  //tambien se puede escribir asi const {offset} = getState.pokemons
+    //    console.log('getSTate trae',getState().pokemons.offset)
+    //    const offset = getState().pokemons.offset  //tambien se puede escribir asi const {offset} = getState.pokemons
     
     
-       getState()
+    //    getState()
     try {
-       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`)
+       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
        dispatch({
            type:OBTENER_POKEMONS_EXITO,
-           payload:res.data.results  //esto lo envia 
+           payload:res.data  //esto lo envia 
        })
     }
     catch (err){
@@ -53,18 +53,35 @@ export const obtenerPokemonsAccion = () => async (dispatch,getState)=>{
 
 export const siguientePokemonAccion = () => async (dispatch,getState) => {
     //primera alternativa
-    const offset = getState().pokemons.offset
-    const siguiente = offset + 20
+    // const offset = getState().pokemons.offset
+    // const siguiente = offset + 20
+    const next = getState().pokemons.next
 try {
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${siguiente}&limit=20`)
+                                                      
+    const res = await axios.get(next)
     dispatch({
         type:SIGUIENTE_POKEMONS_EXITO,
-        payload:{
-           array: res.data.results,
-           offset:siguiente 
-         }
+        payload:res.data 
+         
     })
 } catch (error) {
     console.log(error)
 }
+}
+
+export const anteriorPokemonAccion =()=>async(dispatch,getState)=>{
+    const previous = getState().pokemons.previous
+    
+    try {
+                                                      
+        const res = await axios.get(previous)
+        dispatch({
+            type:SIGUIENTE_POKEMONS_EXITO,
+            payload:res.data 
+             
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
 }
